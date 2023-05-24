7391567c9fe038c84a06bf07e1e5f13f449a6eac
misterpuffin <snghaoren@gmail.com>
2023-05-24 22:48:44 +0800

    Add ChakraUI

--- src/pages/_app.tsx
+++ src/pages/_app.tsx
@@ -3,6 +3,7 @@ import { type Session } from "next-auth";
 import { SessionProvider } from "next-auth/react";
 
 import { api } from "~/utils/api";
+import { ChakraProvider } from "@chakra-ui/react";
 
 import "~/styles/globals.css";
 
@@ -11,9 +12,11 @@ const MyApp: AppType<{ session: Session | null }> = ({
   pageProps: { session, ...pageProps },
 }) => {
   return (
-    <SessionProvider session={session}>
-      <Component {...pageProps} />
-    </SessionProvider>
+    <ChakraProvider>
+      <SessionProvider session={session}>
+        <Component {...pageProps} />
+      </SessionProvider>
+    </ChakraProvider>
   );
 };
 
